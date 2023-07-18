444178cc6e3ad55b53a002d9956e73bdfc05ceee
Amal K Ajith <amalajithk10@gmail.com>
2023-07-18 12:35:17 +0530

    scramble to diff msgs

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 const bub= document.getElementById("bub-wrap");
 const clr=['red','blue','green','yellow','cyan','pink'];
 let i=0;
+let count=0
+let scrambleFlag=false;
 const follow = (x,y)=>{
     let blob=document.getElementById("blob")
     blob.animate({
@@ -36,17 +38,38 @@ window.onmousemove=e=>{
     follow(e.clientX,e.clientY)
 }
 // window.addEventListener("resize",generateCells);
+function changeHeading(){
+    if(scrambleFlag)return;
+    const arr=['Hello Welcome', 'welcome to the site','frontend dev','js','java','react','angular'];
+    document.getElementById('heading').dataset.value=arr[count%arr.length].toUpperCase();
+    scramble();
+    count++;
+}
 
-document.getElementById('heading').onmouseover=event=>{
+function scramble(){
     const letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     let ite=0;
+    const heading=document.getElementById('heading');
     let int=setInterval(()=>{
-        event.target.innerHTML=event.target.innerHTML.split('').map(
+        heading.innerHTML=heading.dataset.value.split('').map(
             (letter,index)=>{
-            if(index<ite)return event.target.dataset.value[index];
+            if(index<ite)return heading.dataset.value[index];
             return letters[Math.floor(Math.random()*26)]
         }).join("");
-        if(ite>event.target.dataset.value.length)clearInterval(int);
+        if(ite>heading.dataset.value.length){
+            setTimeout(changeHeading,1000);
+            clearInterval(int);
+        }
         ite+=1/3;
     },50)
 }
+// document.getElementById('heading').onmouseover=event=>{
+    // }
+document.getElementById('heading').onmouseleave=event=>{
+    scrambleFlag=false;
+    changeHeading();
+}    
+document.getElementById('heading').onmouseover=event=>{
+    scrambleFlag=true;
+}
+(changeHeading)()
