953005550f87c27743180e17e9303a86d5737e4d
Amal K Ajith <amal.ajith@impactanalytics.co>
2024-01-04 16:25:55 +0530

    progress in stopWatch

--- javascript.js
+++ javascript.js
@@ -1,15 +1,22 @@
+
+// import { eleId } from "./helper";
+// const helper = require( "./helper");
+// requi
+// const eleId = helper.eleId;
+function eleId(id){
+    return document.getElementById(id)
+}
+
+
+
+
+
 // Bubble Code
-const bub= document.getElementById("bub-wrap");
+const bub= eleId("bub-wrap");
 const clr=['red','blue','green','yellow','cyan','pink'];
 let i=0;
 let count=0
-const follow = (x,y)=>{
-    let blob=document.getElementById("blob")
-    blob.animate({
-        left: `${x}px`,
-        top: `${y}px`
-    },{duration: 3000, fill:"forwards"})
-}
+
 const fn=e=>{
     const di=document.createElement("div");
     if(i>=clr.length)i=0;
@@ -22,7 +29,7 @@ const fn=e=>{
 
 const generateCells= ()=>{
     console.log("hello there")
-    const cell_cont=document.getElementById("cell-cont");
+    const cell_cont=eleId("cell-cont");
     cell_cont.innerHTML='';
     
     for(let i=0;i<=(window.innerWidth/50)*9;i++){
@@ -37,15 +44,38 @@ const generateCells= ()=>{
 
 // --- Bubble code end ----
 
-let scrambleFlag=false;
+let scrambleFlag = false;
+let runningFlag = false;
 let stopFlag;
+let watchInterval = [];
+let alarmFlag = false;
+let min_timer_value;
+let seconds = 0;
+
+const alarmSound  = new Audio("ringtone-126505.mp3");
+alarmSound.loop= true;
+
+
+const follow = (x,y)=>{
+    let blob=eleId("blob")
+    if(document.title=="Stop Watch"){
+        blob.style.background = "linear-gradient(120deg,rgb(21, 255, 0),#008932)"
+    }
+    if(blob){
+        blob.animate({
+            left: `${x}px`,
+            top: `${y}px`
+        },{duration: 3000, fill:"forwards"})
+    }
+}
 window.onmousemove=e=>{
     follow(e.clientX,e.clientY)
 }
 function changeHeading(){
-    if(scrambleFlag)return;
-    const arr=['Hello Welcome', 'welcome to the site','frontend dev','js','java','react','angular','node'];
-    document.getElementById('heading').dataset.value=arr[count%arr.length].toUpperCase();
+    if(scrambleFlag || runningFlag || !eleId("heading")) return;
+    const arr=['Hello Welcome', 'my name is amal k ajith','frontend dev','js','java','react','angular','node'];
+    eleId('heading').dataset.value=arr[count%arr.length].toUpperCase();
+    runningFlag = true
     scramble();
     count++;
 }
@@ -53,7 +83,7 @@ function changeHeading(){
 function scramble(){
     const letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     let ite=0;
-    const heading=document.getElementById('heading');
+    const heading=eleId('heading');
     let int=setInterval(()=>{
         heading.innerHTML=heading.dataset.value.split('').map(
             (_letter,index)=>{
@@ -63,45 +93,112 @@ function scramble(){
         if(ite>heading.dataset.value.length){
             setTimeout(changeHeading,1000);
             clearInterval(int);
+            runningFlag = false;
         }
         ite+=1/3;
     },50)
 }
-document.getElementById('heading').onmouseleave=event=>{
-    scrambleFlag=false;
-    changeHeading();
-}    
-document.getElementById('heading').onmouseover=event=>{
-    scrambleFlag=true;
+if(eleId('heading')){
+    eleId('heading').onmouseleave=event=>{
+        scrambleFlag=false;
+        changeHeading();
+    }    
+    eleId('heading').onmouseover=event=>{
+        scrambleFlag=true;
+    }
 }
-(changeHeading)()
 
-function startWatch(stop=false){
- stopFlag = stop;
- let min = document.getElementById("min");
- let sec = document.getElementById("sec");
- let click = document.getElementById("click_me");
- let stopId = document.getElementById("stop");
- if(!stop){
-     click.style.display = "none";
-     stopId.style.display = "block";
-    } 
-else {
-    stopId.style.display = "none";
-    click.style.display = "block";
- }
- let seconds=0;
- let interval = setInterval(() => {
-     if(stopFlag){
-        clearInterval(interval);
-        return
-     }
-    seconds = +sec.innerHTML + 1;
-    if(sec.innerHTML>=59)min.innerHTML= formatNumber((seconds%60)+1);
-    sec.innerHTML= formatNumber(seconds%60);
- },1000)
+function startWatch(stop = false) {
+    stopFlag = stop;
+    let min = eleId("min");
+    let sec = eleId("sec");
+    let click = eleId("play_child");
+    let click_parent = eleId("play_parent");
+    let stopId = eleId("stop_child");
+    let stopId_parent = eleId("stop_parent");
+    if (!stop) {
+        click.style.display = "none";
+        click_parent.style.display = "none";
+        stopId.style.display = "block";
+        stopId_parent.style.display = "block";
+    }
+    else {
+        stopId.style.display = "none";
+        stopId_parent.style.display = "none";
+        click.style.display = "block";
+        click_parent.style.display = "block";
+    }
+    
+    if (stopFlag) {
+        watchInterval.forEach(clearInterval);
+        return;
+    }
+    watchInterval.push(setInterval(() => {
+         if(seconds>=min_timer_value && min_timer_value>0){
+            playSound();
+            let stop_sound = eleId("stop_sound");
+            stop_sound.style.display="block";
+            stopFlag=true
+            watchInterval.forEach(clearInterval);
+            return
+         }
+        seconds = ++seconds;
+        if (sec.innerHTML >= 59) min.innerHTML =  formatNumber(Number(min.innerHTML)+((seconds % 60) + 1));
+        sec.innerHTML = formatNumber(seconds % 60);
+    }, 1000))
 }
+
+function setTimer(){
+    let timer_value = eleId("timer").value;
+    console.log(timer_value);
+}
+
+function setTimerMin (sign){
+    let min_ele = eleId("timer_min");
+    if(min_ele.innerHTML<='0' && sign=='-')return;
+    if(sign === '-')min_ele.innerHTML=(+min_ele.innerHTML)-1;
+    if(sign === '+')min_ele.innerHTML=(+min_ele.innerHTML)+1;
+    min_timer_value = +min_ele.innerHTML*60
+}
+
+function playSound() {
+    if (!alarmFlag) {
+        alarmSound.play();
+        alarmFlag = true;
+    }
+    else{
+        alarmSound.pause();
+        let stop_sound= eleId("stop_sound");
+        stop_sound.style.display="none";
+        resetWatch()
+        alarmFlag=false;
+    } 
+}
+
+function resetWatch(){
+    let min = eleId("min");
+    let sec = eleId("sec");
+    min.innerHTML="00";
+    sec.innerHTML="00";
+    seconds=0;
+
+    let click = eleId("play_child");
+    let click_parent = eleId("play_parent");
+    let stopId = eleId("stop_child");
+    let stopId_parent = eleId("stop_parent");
+    
+    stopId.style.display = "none";
+    stopId_parent.style.display = "none";
+    click.style.display = "block";
+    click_parent.style.display = "block";
+    
+}
+
 
 function formatNumber(number){
     return number<=9 ? "0"+number :number
+}
+
+window.onload = (event) =>{
+     (changeHeading)()
 }
